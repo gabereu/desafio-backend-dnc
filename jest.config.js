d20b58c6f9779459a3462e531f967d5bdffcb066
Gabriel Santos <gabriel.s.resende99@gmail.com>
2022-07-27 14:45:50 -0300

    Change to commonjs to better compatibility with swc jsc.paths

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-export default {
+module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "../coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -121,7 +121,7 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  rootDir: "./src",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -175,12 +175,7 @@ export default {
   // A map from regular expressions to paths to transformers
     transform: {
       "^.+\\.(t|j)s$": [
-        "@swc/jest",
-        {
-          jsc: {
-            target: "es2021",
-          },
-        },
+        "@swc/jest"
       ],
     },
 
